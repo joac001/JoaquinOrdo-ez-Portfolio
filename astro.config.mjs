569c5d4fd76fe44ee6360aad3cfbf7e8227193a2
joac001 <jordonez@fi.uba.ar>
2024-11-07 10:50:11 -0300

    trying configs to work

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://joac001.github.io',
-  base: './JoaquinOrdonez-Portfolio',
+
   integrations: [tailwind(), react()]
 });
