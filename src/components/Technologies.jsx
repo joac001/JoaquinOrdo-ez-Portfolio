4c101705be34cac1ac5360547e1e8d6b22056798
joac001 <jordonez@fi.uba.ar>
2024-11-07 19:14:46 -0300

    deploy

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,16 +1,14 @@
 import { technologies } from "../data/technologies.js";
 
 export default function Technologies() {
-
     return (
         <span className="flex gap-10 flex-wrap w-full text-white font-sans justify-center items-center p-4">
             {
                 technologies.map((tech, index) => {
-
                     return (
                         <span key={index} className="grid grid-rows-2 items-center justify-center m-2 w-20 h-20 gap-2">
                             {
-                                <img src={`src/assets/${tech.icon}`} alt={tech.name} className="w-16" loading="eagerly" />
+                                <img src={`/assets/${tech.icon}`} alt={tech.name} className="w-16" loading="eagerly" />
                             }
                             <span className="text-center text-sm font-semibold mt-1">{tech.name}</span>
                         </span>
