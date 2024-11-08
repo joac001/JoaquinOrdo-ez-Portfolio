35e63e6bf4aae338a4a5a50f241a5161086f4f09
joac001 <jordonez@fi.uba.ar>
2024-11-08 12:14:23 -0300

    deploy

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -2,7 +2,7 @@ import { technologies } from "../data/technologies.js";
 
 export default function Technologies() {
 
-    const baseUrl = `${import.meta.env.BASE_URL}`
+    const baseUrl = import.meta.env.BASE_URL || "/";
 
     return (
         <span className="flex gap-10 flex-wrap w-full text-white font-sans justify-center items-center p-4">
@@ -10,9 +10,9 @@ export default function Technologies() {
                 technologies.map((tech, index) => {
                     return (
                         < span key={index} className="grid grid-rows-2 items-center justify-center m-2 w-20 h-20 gap-2" >
-                            {
-                                <img src={`../../public/assets/${tech.icon}`} alt={tech.name} className="w-16" loading="eagerly" />
-                            }
+
+                            <img src={`${baseUrl}assets/${tech.icon}`} alt={tech.name} className="w-16" loading="eager" />
+
                             <span className="text-center text-sm font-semibold mt-1">{tech.name}</span>
                         </span>
                     );
